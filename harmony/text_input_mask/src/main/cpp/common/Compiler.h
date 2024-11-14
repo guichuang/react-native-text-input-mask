f404a5b1005e016533968b0943830e58a38171ac
guichuang <13343468815@163.com>
2024-11-14 11:55:30 +0800

    fix：修复isOptional为false时会闪退的问题

--- harmony/text_input_mask/src/main/cpp/common/Compiler.h
+++ harmony/text_input_mask/src/main/cpp/common/Compiler.h
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #pragma once
 #include <string>
 #include <vector>
@@ -105,23 +128,19 @@ public:
     }
 
     std::unique_ptr<State> compileWithCustomNotations(char c, const std::string &str) {
-         DLOG(ERROR) << "====== compileWithCustomNotations  customNotations size: " << customNotations.size();
         for (const auto &customNotation : customNotations) {
             if (customNotation.character == c) {
                 std::shared_ptr<State> compiledState = compile(str.substr(1), true, false, c);
                 if (customNotation.isOptional) {
-                    DLOG(ERROR) << "====== compileWithCustomNotations  customNotations isOptional :" << customNotation.isOptional;
                     return std::make_unique<OptionalValueState>(
                         std::move(compiledState),
                         std::make_shared<OptionalValueState::Custom>(c, customNotation.characterSet));
                 } else {
-                     DLOG(ERROR) << "======= compileWithCustomNotations  customNotations isOptional :" << customNotation.isOptional;
                     return std::make_unique<ValueState>(
                         std::move(compiledState), std::make_shared<ValueState::Custom>(c, customNotation.characterSet));
                 }
             }
         }
-        DLOG(INFO) << "compileWithCustomNotations No Match Found ";
         throw FormatError("compileWithCustomNotations No Match Found");
     }
     std::shared_ptr<ValueState::ValueStateType> determineInheritedType(std::optional<char> lastCharacter) {
@@ -153,7 +172,6 @@ public:
         if (!lastCharacter.has_value()) {
             throw FormatError(); // 处理空字符情况，抛出异常
         }
-         DLOG(INFO) << "======determineTypeWithCustomNotations customNotations size: "<<customNotations.size();
         char character = lastCharacter.value();
         for (const auto &customNotation : customNotations) {
             if (customNotation.character == character) {
@@ -162,7 +180,6 @@ public:
                                                             customNotation.characterSet); // 可以根据需要调整返回内容
             }
         }
-        DLOG(INFO) << "determineTypeWithCustomNotations No Match Found ";
         throw FormatError("determineTypeWithCustomNotations No Match Found"); // 未找到匹配项，抛出异常
     }
 };
--- harmony/text_input_mask/src/main/cpp/common/Mask.h
+++ harmony/text_input_mask/src/main/cpp/common/Mask.h
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #pragma once
 #include <string>
 #include <vector>
@@ -321,7 +344,7 @@ private:
             } else if (optionalValueState->type->getName() == StateTypeName::AlphaNumeric) {
                 return appendPlaceholder(state->child.get(), placeholder + "-");
             } else if (optionalValueState->type->getName() == StateTypeName::Custom) {
-                auto customValueState = dynamic_cast<OptionalValueState *>(state);
+                auto customValueState = dynamic_cast<OptionalValueState *>(state->child.get());
                 auto customStateType = dynamic_cast<OptionalValueState::Custom *>(customValueState->type.get());
                 return appendPlaceholder(state->child.get(), placeholder + customStateType->character);
             } else {
@@ -341,14 +364,9 @@ private:
             } else if (valueState->type->getName() == StateTypeName::AlphaNumeric) {
                 return appendPlaceholder(state->child.get(), placeholder + "-");
             } else if (valueState->type->getName() == StateTypeName::Custom) {
-                auto customValueState = dynamic_cast<OptionalValueState *>(state);
-                if (customValueState->type.get()) {
-                    auto customStateType = dynamic_cast<OptionalValueState::Custom *>(customValueState->type.get());
-                    return appendPlaceholder(state->child.get(), placeholder + customStateType->character);
-                }else {
-                    throw FormatError("appendPlaceholder customValueState type is null"); // 未找到匹配项，抛出异常 
-                }
-
+                auto customValueState = dynamic_cast<ValueState *>(state);
+                auto customStateType = dynamic_cast<ValueState::Custom *>(customValueState->type.get());
+                return appendPlaceholder(state->child.get(), placeholder + customStateType->character);
             } else {
                 return placeholder; // 未知类型，返回原始 placeholder
             }
